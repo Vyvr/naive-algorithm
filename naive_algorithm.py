2377c408aa7ecdafa00cb3100b34288c50425341
Maciej Luciński <50800058+Vyvr@users.noreply.github.com>
2022-10-11 12:16:35 +0200

    Add files via upload

---
+++ naive_algorithm.py
@@ -0,0 +1,34 @@
+
+
+from operator import truediv
+
+
+# ----Finds if pattern matches text, starting from index----#
+def matchesAt(text, index, pattern):
+    if len(text) < len(pattern):
+        return False
+    for i in range(0, len(pattern)):
+        if text[index + i] != pattern[i]:
+            return False
+    return True
+
+# ----Adds to array indexes where pattern matches text
+
+
+def report(index, matchedIndexes):
+    matchedIndexes.append(index)
+
+
+def main():
+    text = 'acbabadababbdbabac'
+    pattern = 'babac'
+    matchedIndexes = []
+    for i in range(0, (len(text) - len(pattern)) + 1):
+        if matchesAt(text, i, pattern):
+            report(i, matchedIndexes)
+
+    print(matchedIndexes)
+
+
+if __name__ == "__main__":
+    main()
